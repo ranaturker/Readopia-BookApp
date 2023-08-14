73d0b42f3fd46586626ed32ecb1f34e8255052a3
ranaturker <turkerm@mef.edu.tr>
2023-08-14 16:29:01 +0300

    Fix the DetailFragment

--- app/src/main/java/com/ranaturker/readopia/ui/DetailFragment.kt
+++ app/src/main/java/com/ranaturker/readopia/ui/DetailFragment.kt
@@ -1,45 +1,35 @@
 package com.ranaturker.readopia.ui
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import com.ranaturker.readopia.R
-import com.ranaturker.readopia.network.Result
+import com.ranaturker.readopia.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
-    private var book: Result ?= null
+    private lateinit var binding: FragmentDetailBinding
     private val viewModel: DetailViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+    ): View {
+        binding = FragmentDetailBinding.inflate(layoutInflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        if (arguments != null) {
-            val bookId = DetailFragmentArgs.fromBundle(requireArguments()).bookId
-            if(bookId!=-1){
-                viewModel.getBookWithId(bookId)
+        val bookId = DetailFragmentArgs.fromBundle(requireArguments()).bookId
+        if (bookId != -1) {
+            viewModel.getBookWithId(bookId)
+        }
+        viewModel.book.observe(viewLifecycleOwner) { book ->
+            if (book != null) {
+                binding.name.text = book.title
             }
         }
-        viewModel.book.observe(viewLifecycleOwner) {
-            if (it != null) {
-                book = it
-                Log.d("detail book", book.toString())
-            }
-        }
-
     }
-
 }
