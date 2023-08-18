94917ccbf08db87a528bf65abd9121a31cdb48fe
ranaturker <turkerm@mef.edu.tr>
2023-08-18 12:39:04 +0300

    Add splash screen and required dependencies

---
+++ app/src/main/java/com/ranaturker/readopia/ui/SplashScreenFragment.kt
@@ -0,0 +1,28 @@
+package com.ranaturker.readopia.ui
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.ranaturker.readopia.R
+
+class SplashScreenFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(SplashScreenFragmentDirections.actionSplashScreenFragmentToListFragment())
+        }, 4561)
+    }
+}
